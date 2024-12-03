c43a6f08a3966f70fbfccdc105c656a2f40b17f6
“John <john.danial@me.com>
2024-12-02 21:23:46 -0600

    Labs

--- Menu/index.js
+++ Menu/index.js
@@ -1,27 +1,28 @@
 
 const menuButton = document.getElementById("menu-button")
 const menu = document.getElementById("menu")
-const option1 = document.getElementById("Letters")
-const option2 = document.getElementById("Numbers")
-const page1 = document.getElementById("letters-page")
-const page2 = document.getElementById("numbers-page")
+const letters = document.getElementById("Letters")
+const numbers = document.getElementById("Numbers")
+const lettersPage = document.getElementById("letters-page")
+const numbersPage = document.getElementById("numbers-page")
 
 menuButton.addEventListener("click", () => {
     menu.style.display = menu.style.display === "block" ? "none" : "block" })
 
-    option1.addEventListener("click", (e) => {
-        e.preventDefault()
+letters.addEventListener("click", (e) => {
+    e.preventDefault()
    
-    page1.classList.add("active")
-    page2.classList.remove("active")
+    lettersPage.classList.add("active")
+    numbersPage.classList.remove("active")
 
     menu.style.display = "none"
 })
 
-option2.addEventListener("click", (e) => {
+numbers.addEventListener("click", (e) => {
     e.preventDefault()
-    page2.classList.add("active")
-    page1.classList.remove("active")
+
+    numbersPage.classList.add("active")
+    lettersPage.classList.remove("active")
 
     menu.style.display = "none"
 })
